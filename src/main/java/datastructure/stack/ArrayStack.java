ee7ab8d6c3949baa5788e65b73ae614ece88112b
liueleven <liueleven@aliyun.com>
2019-02-22 01:30:18 +0800

    数组栈的实现

---
+++ src/main/java/datastructure/stack/ArrayStack.java
@@ -0,0 +1,73 @@
+package datastructure.stack;
+
+import java.util.Arrays;
+
+/**
+ * @description: 基于数组的栈
+ *                  栈的特点：
+ *                      先进先出，后进后出
+ *                      插入和删除都在一端
+ * @date: 2019-02-22 00:34
+ * @author: 十一
+ */
+public class ArrayStack<T> {
+
+    private T[] stacks;
+
+    /**
+     * 栈的数量
+     */
+    private int count;
+
+    /**
+     * 栈的容量
+     */
+    private int capacity;
+
+    public ArrayStack(T[] stacks, int count, int capacity) {
+        this.stacks = stacks;
+        this.count = count;
+        this.capacity = capacity;
+    }
+
+    public void printall() {
+        if(this.stacks == null) {
+            return;
+        }
+        for(int i=this.stacks.length-1; i>=0; i--) {
+            if(stacks[i] != null) {
+                System.out.println(stacks[i]);
+            }
+        }
+    }
+
+    public void push(T data) {
+        if(count >= this.stacks.length) return;
+        this.stacks[count] = data;
+        count ++;
+    }
+
+    public void pop() {
+        this.stacks[count-1] = null;
+        count --;
+    }
+
+    public static void main(String[] args) {
+        ArrayStack<Integer> stack = new ArrayStack<Integer>(new Integer[10],0,10);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+        stack.push(8);
+        stack.push(9);
+        stack.push(10);
+        stack.pop();
+        stack.push(11);
+        stack.pop();
+        stack.printall();
+    }
+
+}
